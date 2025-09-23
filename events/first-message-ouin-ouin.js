f2fa4b5848e4aec6315681203368173cba2ce2df
myreage <remy.vandyk@gmail.com>
2025-09-23 13:52:33 +0200

    salut fraicheur

---
+++ events/first-message-ouin-ouin.js
@@ -0,0 +1,31 @@
+const { Events } = require("discord.js");
+
+const isAtLeastOneDayAfter = (firstDate, secondDate) => {
+  const d1 = new Date(
+    firstDate.getFullYear(),
+    firstDate.getMonth(),
+    firstDate.getDate()
+  );
+  const d2 = new Date(
+    secondDate.getFullYear(),
+    secondDate.getMonth(),
+    secondDate.getDate()
+  );
+
+  return d2.getTime() > d1.getTime();
+};
+
+let lastMessageDate = new Date();
+
+module.exports = {
+  name: Events.MessageCreate,
+  execute(message) {
+    const createdAt = new Date(message.createdAt);
+
+    if (isAtLeastOneDayAfter(lastMessageDate, createdAt)) {
+      message.reply("Salut fraîcheur");
+    }
+
+    lastMessageDate = createdAt;
+  },
+};
